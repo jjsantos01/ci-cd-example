89b4b72f3fab785b0b2044ec9e03b5f80e902b33
Juan Santos <jjsantosochoa@outlook.com>
2024-06-27 18:26:37 -0600

    change greetings

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ from api.fast import root
 
 st.write(root())
 
-st.write('This is obviously very simple, in reality one would rather use this page to display API results more nicely! :)')
+st.write('Qué onda, plebada! \nThis is obviously very simple, in reality one would rather use this page to display API results more nicely! :)')
